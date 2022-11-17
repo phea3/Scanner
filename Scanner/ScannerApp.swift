af5ea11283947fa809d0e434bcb2187f4ecebe8d
loun sokphea <lounsokphea99@gmail.com>
2022-11-17 17:12:22 +0700

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Luon Sokphea on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
